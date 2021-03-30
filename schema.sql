7f173e7eb332fe668f8e93558c43af17b569d285
talahajeer <tala.hajeer@gmail.com>
2021-03-30 23:08:07 +0300

    kickoff lab08

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE location (search_query VARCHAR(256) NOT NULL,
+                formatted_query VARCHAR(256) NOT NULL,
+                latitude FLOAT NOT NULL,
+                longitude: FLOAT NOT NULL
+                )
+
