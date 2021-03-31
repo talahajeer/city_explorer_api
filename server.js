276a1d4170541f8b0708ca9ada2d97c994b19848
talahajeer <tala.hajeer@gmail.com>
2021-03-31 15:05:36 +0300

    lab07

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ let lon = "";
 const localLocations = {};
 function handleLocation(request, response) {
     const city = request.query.city;
-    console.log(city);
+    // console.log(city);
     if (localLocations[city]) {
         response.send(localLocations[city]);
     } else {
@@ -43,7 +43,7 @@ function handleLocation(request, response) {
             }
             lat = res.body[0].lat;
             lon = res.body[0].lon;
-            console.log(object);
+            // console.log(object);
             response.send(object);
         }).catch((err) => {
             console.log("ERROR IN LOCATION API");
@@ -58,16 +58,18 @@ const weatherResponse = {};
 function handleWeather(request, response) {
     let resArr = [];
     const city = request.query.city;
+    console.log(request.query);
     if (weatherResponse[city]) {
         response.send(weatherResponse[city])
     } else {
         let key = process.env.WEATHER_API_KEY;
-        const url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${key}`;
+        // lat = res.body[0].lat;
+        // lon = res.body[0].lon;
+        // console.log(lat,lon);
+        const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
         superagent.get(url).then(res => {
-            // console.log(res.body);
-            // const getWeather = require("./data/weather.json");
             const cityWeather = res.body.data;
-            console.log(res.body.data);
+            console.log(res.body);
             cityWeather.forEach(item => {
 
                 resArr.push({
@@ -78,7 +80,7 @@ function handleWeather(request, response) {
             // console.log(weatherResponse);
             response.send(resArr);
         }).catch((err) => {
-            console.log("ERROR IN LOCATION API");
+            console.log("ERROR IN WEAHTER API");
             console.log(err)
         })
     }
@@ -90,7 +92,7 @@ const parkResponse = {};
 function handlePark(request, response) {
     let resArr = [];
     const city = request.query.city;
-    console.log(city);
+    // console.log(city);
     if (parkResponse[city]) {
         response.send(parkResponse[city])
     } else {
@@ -105,7 +107,7 @@ function handlePark(request, response) {
                     name: item.fullName,
                     address: item.addresses[0],
                     fees: item.fees,
-                    description:item.description,
+                    description: item.description,
                     url: item.url
                 });
             })
@@ -114,7 +116,7 @@ function handlePark(request, response) {
             console.log(resArr);
             response.send(resArr);
         }).catch((err) => {
-            console.log("ERROR IN LOCATION API");
+            console.log("ERROR IN PARKS API");
             console.log(err)
         })
     }
