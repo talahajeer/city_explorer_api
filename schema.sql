5db00fea98dc38323619bb4a3367c1910cedaf0b
talahajeer <tala.hajeer@gmail.com>
2021-04-03 13:16:46 +0300

    finished editting-to deploy

--- schema.sql
+++ schema.sql
@@ -4,11 +4,13 @@ drop table if exists Parks;
 
 
 
-CREATE  TABLE IF NOT EXISTS location (name VARCHAR(255), display_name VARCHAR(255),latitude FLOAT ,longitude FLOAT);
+CREATE  TABLE IF NOT EXISTS location (
+    id SERIAL NOT NULL,
+    search_query VARCHAR(256),
+    formatted_query VARCHAR(256),
+    latitude FLOAT(24),
+    longitude FLOAT(24)
+    );
 
-CREATE  TABLE IF NOT EXISTS weather (period VARCHAR(155),forecast VARCHAR (255));
 
-CREATE TABLE IF NOT EXISTS Parks (name VARCHAR(255),url VARCHAR(255),fee VARCHAR(10),description VARCHAR(255));
 
-CREATE TABLE IF NOT EXISTS movies(search_query VARCHAR(256),title VARCHAR(265),overview VARCHAR(1000),average_votes FLOAT,total_votes INT,image_url VARCHAR(265),popularity FLOAT,released_on VARCHAR(256)
-);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 "use strict";
+
 require("dotenv").config();
 const PORT = process.env.PORT;
 
@@ -6,7 +7,7 @@ const express = require("express");
 const cors = require("cors");
 const superagent = require("superagent");
 const pg = require("pg");
-const { query } = require("express");
+
 
 const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -17,29 +18,23 @@ client.connect().then(() => {
     app.listen(PORT, () => console.log(`App is running on Server on port: ${PORT}`))
 });
 
-app.get('/', (request, response) => {
-    let name = request.query.name;
-    let SQL = 'SELECT * FROM location WHERE name=$1';
-    client.query(SQL, [name]).then(result => {
-        response.send(result.rows);
-    });
-});
 
 
 app.get("/location", handleLocation);
 app.get("/weather", handleWeather);
 app.get("/parks", handlePark);
 app.get('/movies', handleMovies);
+app.get('/yelp', handleyelp);
 app.use('*', notFoundHandler);
-app.use(errorHandler);
+// app.use(errorHandler);
 
 function notFoundHandler(request, response) {
     response.status(404).send('requested API is Not Found!');
 }
 
-function errorHandler(err, request, response, next) {
-    response.status(500).send('something is wrong in server');
-}
+// function errorHandler(err, request, response, next) {
+//     response.status(500).send('something is wrong in server');
+// }
 let lat = "";
 let lon = "";
 
@@ -53,20 +48,17 @@ function handleLocation(request, response) {
 
     let key = process.env.GEOCODE_API_KEY;
 
-    let SQL = `SELECT * FROM location WHERE name=${city}`;
+    let SQL = `SELECT * FROM location WHERE search_query = '${city}';`;
 
-    let SQLInsertion = 'INSERT INTO location (name, display_name,latitude,longitude) VALUES($1, $2, $3, $4) RETURNING *';
+    let SQLInsertion = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4) RETURNING *;';
 
-    client.query(SQL).then(data => {
-        data.rows.forEach(value => {
-            localLocations[value.search_query] = value;
-        });
-    });
 
-    client.query(SQL, [city]).then(result => {
+    client.query(SQL).then(result => {
         if (result.rowCount > 0) {
-            // console.log(result.rows[0]);
-            response.send(localLocations[city]);
+            lat = result.rows[0].latitude;
+            lon = result.rows[0].longitude;
+            // console.log(lat,lon);
+            response.send(result.rows[0]);
         } else {
             const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
 
@@ -80,7 +72,7 @@ function handleLocation(request, response) {
                 }
                 lat = res.body[0].lat;
                 lon = res.body[0].lon;
-
+                // console.log(lat, lon);
                 let values = [city, res.body[0].display_name, lat, lon];
                 client.query(SQLInsertion, values).then(() => {
                     localLocations[city] = object;
@@ -99,110 +91,116 @@ function handleLocation(request, response) {
 
 const weatherResponse = {};
 function handleWeather(request, response) {
-
-    const city = request.query.search_query;
+    let resArr = [];
+    const city = request.query.city;
     let key = process.env.WEATHER_API_KEY;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${key}`;
 
-    let SQLInsertion = 'INSERT INTO weather (period, forecast) VALUES($1, $2) RETURNING *';
+    superagent.get(url).set('Authorization', `Bearer ${key}`).then(res => {
+        const cityWeather = res.body.data;
+        // console.log(res);
+        cityWeather.forEach(item => {
+            let object = {
+                forecast: item.valid_date,
+                time: item.weather.description
+            }
+            resArr.push(object);
+        });
+        response.send(resArr);
+    }).catch((err) => {
+        console.log("ERROR IN WEATHER API");
+        console.log(err)
+    })
+};
 
-    let SQL = `SELECT * FROM weathers WHERE period=$1`;
-
-    client.query(SQL, [city]).then(result => {
-        if (result.rowCount > 0) {
-            response.send(result.rows);
-        } else {
-            const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&country=US&key=${key}`;
-
-            superagent.get(url).then(res => {
-
-                const cityWeather = res.body.data;
-                // console.log(res.body.data);
-                cityWeather.forEach(item => {
-                    let values = [item.valid_date, item.weather.description];
-
-                    client.query(SQLInsertion, values);
-                });
-                client.query(SQL, [city]).then(data => {
-                    response.send(data.rows);
-                })
-            }).catch((err) => {
-                console.log("ERROR IN WEATHER API");
-                console.log(err)
-            })
-        }
-    });
-
-
-const parkResponse = {};
 function handlePark(request, response) {
-
-    const city = request.query.search_query;
+    let resArr = [];
+    const city = request.query.city;
+    // console.log(request.query);
     let key = process.env.PARKS_API_KEY;
 
-    let SQL = 'SELECT * FROM Parks WHERE name=$1';
-    let SQLInsertion = 'INSERT INTO Parks (name,url,fee,descrition) VALUES($1, $2,$3,$4) RETURNING *';
+    const url = `https://developer.nps.gov/api/v1/parks?api_key=${key}&limit=10`;
 
-    client.query(SQL, [city]).then(result => {
-        if (result.rowCount > 0) {
-            response.send(result.rows);
-        } else {
-            const url = `https://developer.nps.gov/api/v1/parks?q=${city}&api_key=${key}&limit=10`;
-
-            superagent.get(url).then(res => {
-
-                const cityPark = res.body.data;
-                // console.log(cityPark);
-                cityPark.forEach(item => {
-                    let values = [item.fullName, item.url, item.fees, item.description];
-
-                    client.query(SQLInsertion, values);
-
-                })
-                client.query(SQL, [city]).then(data => {
-                    response.send(data.rows)
-                })
-            }).catch((err) => {
-                console.log("ERROR IN PARKS API");
-                console.log(err)
-            })
-        }
+    superagent.get(url).then((res) => {
+        const cityPark = res.body.data;
+        // console.log("from park superaget-----------",cityPark);
+        cityPark.forEach(item => {
+            let object = {
+                name: item.fullName,
+                address: item.addresses[0].line1,
+                fee: item.entranceFees[0].cost,
+                description: item.description,
+                url: item.url
+            }
+            resArr.push(object);
+            // console.log(resArr);
+        });
+        response.send(resArr)
+    }).catch((err) => {
+        console.log("ERROR IN PARKS API");
+        console.log(err)
     })
 }
 
 
+
 function handleMovies(request, response) {
+    let resArr = [];
     let key = process.env.MOVIE_API_KEY;
     const city = request.query.search_query;
-
-    let SQLInsertion = 'INSERT INTO movies (search_query,title,overview ,average_votes,total_votes,image_url,popularity,released_on) VALUES($1, $2, $3, $4, $5,$6,$7,$8) RETURNING *';
-    let SQL = 'SELECT * FROM movies WHERE search_query=$1';
-
+    // console.log(city);
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}`;
-    client.query(SQL, [city]).then(data => {
-        if (data.rowCount > 0) {
-            response.send(data.rows.slice(0, 20));
-        } else {
-            superagent.get(url).then(data => {
-                data.body.results.slice(0, 20).forEach(elem => {
-                    client.query(SQLInsertion, [city,
-                        elem.title,
-                        elem.overview,
-                        elem.vote_average,
-                        elem.vote_count,
-                        `https://image.tmdb.org/t/p/w500${elem.poster_path}`,
-                        elem.popularity,
-                        elem.release_date
-                    ]);
-                });
-                client.query(SQL, [city]).then(data => {
-                    response.send(data.rows);
-                });
-            }).catch((err) => {
-                console.log("ERROR IN PARKS API");
-                console.log(err)
-            })
-        }
+
+    superagent.get(url).set("Authorization", `Bearer ${key}`).then(res => {
+        let moviesData = res.body.results;
+        console.log("dataaaaaaaaaaaaaaaaaaaaaaa",moviesData);
+        moviesData.forEach(item => {
+            let object = {
+                title: item.title,
+                overview: item.overview,
+                average: item.vote_average,
+                total_votes: item.vote_count,
+                image_url: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                popularity: item.popularity,
+                released_on: item.release_date,
+            };
+            resArr.push(object);
+        });
+        // console.log(resArr);
+        response.send(resArr);
+    }).catch((err) => {
+        console.log("ERROR IN MOVIES API");
+        console.log(err)
     });
 }
 
+function handleyelp(request, response) {
+    let startIndex = 0;
+    let resArr = [];
+    let key = process.env.YELP_API_KEY;
+    const city = request.query.location;
+    // console.log(request.query);
+    const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}&limit=25`;
 
+    superagent.get(url).set("Authorization", `Bearer ${key}`).then(data => {
+        let yelpData = data.body.businesses;
+        yelpData.forEach(element => {
+            let object = {
+                name: element.name,
+                image_url: element.image_url,
+                price: element.price,
+                rating: element.rating,
+                url: element.url
+            }
+            resArr.push(object);
+        });
+    }).then(x => {
+        let endIndex = startIndex + 5;
+        const result = resArr.slice(startIndex, endIndex);
+        startIndex += 5;
+        response.send(result);
+    }).catch((err) => {
+        console.log("ERROR IN MOVIES API");
+        console.log(err)
+    });
+}
